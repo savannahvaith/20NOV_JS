46dcaac4573644046c0138a3cee4539045141d3d
savannahvaith <sav.vaithilingam@gmail.com>
2021-01-13 17:03:12 +0000

    SV: added promises example

--- js/06-Callback-and-promises/promises.js
+++ js/06-Callback-and-promises/promises.js
@@ -1,23 +1,4 @@
 'use strict'; 
-
-// callbacks - a function that takes in a function
-
-// first function
-const greeting = (fullname) => {
-    alert(`Hello ${fullname}`);
-}
-
-// second function 
-const processUserInput = (callback) => {
-    // get the user to type their name 
-    let humansname = prompt("Please enter your name"); 
-    // once the user has entered their name 
-    callback(humansname);
-}
-
-processUserInput(greeting);
-
-
 // resvole = .then();
 // reject = .catch();
 
