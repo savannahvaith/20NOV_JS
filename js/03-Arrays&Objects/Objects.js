d9f355ecbc7763c0f272420c3dadeb711e368d0e
savannahvaith <sav.vaithilingam@gmail.com>
2021-01-13 12:41:46 +0000

    SV: Objects explained

---
+++ js/03-Arrays&Objects/Objects.js
@@ -0,0 +1,60 @@
+'use strict';
+
+// METHOD 1: accessing the object() class (
+let student = new Object();
+// Adding properties to your object
+student.name = "Savannah";
+student["id"] = 123;
+// student.name = "Nick"; // Not best practise to override an existing property
+
+// Method 2:
+let savannah = { name: "Sav", id: 123, courseCode: "JSFUND" };
+
+for (let temp in savannah) {
+    console.log(savannah[temp]);
+}
+
+console.log(student);
+console.log(savannah);
+
+console.log(savannah.name);
+console.log(savannah['id']);
+
+const classroom = [
+    { name: "sav", id: 123, courseCode: "JSFUND" },
+    { name: "jim", id: 234, courseCode: "JSFUND" },
+    { name: "bob", id: 345, courseCode: "JSFUND" },
+    { name: "tom", id: 456, courseCode: "JSFUND" },
+    34,
+    true,
+    false
+]
+
+for (let i = 0; i < classroom.length; i++) {
+    console.log(`the current index is: ${i}, ${classroom[i]}`);
+    for (let tempvariablename in classroom[i]) {
+        console.log(`Values from object: ${classroom[i][tempvariablename]}`);
+    }
+}
+
+// JSON
+let myJSONObject = {
+    "Users": [
+        { "username": "jimbob123", "password": "hello123" },
+        { "username": "bobjim123", "password": "123hello" }
+    ]
+}
+
+// JSON PARSE = Takes in a string, and spits out an object
+let strObj = `{"name":"sav"}`; 
+console.log(typeof strObj);
+
+let obj = JSON.parse(strObj);
+console.log(typeof obj);
+
+
+// JSON STRINGIFY = Takes in a object, and spits out a string. 
+console.log(obj);
+let convertmeback = JSON.stringify(obj);
+console.log(convertmeback);
+console.log(typeof convertmeback);
