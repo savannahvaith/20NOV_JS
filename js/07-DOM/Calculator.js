6e61e08e03e76590166c10466f35e42d7de43754
savannahvaith <sav.vaithilingam@gmail.com>
2021-01-14 10:24:21 +0000

    SV: Completed Calculator Example

---
+++ js/07-DOM/Calculator.js
@@ -0,0 +1,58 @@
+const num1 = document.querySelector("#firstNum");
+const num2 = document.querySelector("#secondNum");
+const result = document.querySelector("#result");
+const history = document.querySelector("#history");
+
+const getValues = () => {
+    return [Number.parseFloat(num1.value), Number.parseFloat(num2.value)];
+}
+
+const addToHistory = (num1, operator, num2, result) => {
+    const addParagraph = document.createElement("p");
+    addParagraph.className = "styleME";
+    const info = document.createTextNode(`${num1} ${operator} ${num2} = ${result}`);
+    addParagraph.appendChild(info);
+    history.appendChild(addParagraph);
+}
+
+const printResult = (newValue) => {
+    result.value = newValue;
+}
+
+const clearHistory = () =>{
+    history.innerHTML = ""; 
+}
+
+const clearValues = () => {
+    num1.value = ""; 
+    num2.value = ""; 
+    result.value = ""; 
+}
+
+const add = () => {
+    const [num1, num2] = getValues();
+    const newResult = num1 + num2;
+    printResult(newResult);
+    addToHistory(num1, "+", num2, newResult);
+}
+
+const subtract = () => {
+    const [num1, num2] = getValues();
+    const newResult = num1 - num2;
+    printResult(newResult);
+    addToHistory(num1, "-", num2, newResult);
+}
+
+const multiply = () => {
+    const [num1, num2] = getValues();
+    const newResult = num1 * num2;
+    printResult(newResult);
+    addToHistory(num1, "X", num2, newResult);
+}
+
+const divide = () => {
+    const [num1, num2] = getValues();
+    const newResult = num1 / num2;
+    printResult(newResult);
+    addToHistory(num1, "/", num2, newResult);
+}
